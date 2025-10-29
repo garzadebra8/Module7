6f3b20cd8b75e471d6c16ced83f69ebafbc4e257
garzadebra8 <garzadebra8@gmail.com>
2025-10-28 19:29:10 -0500

    new

---
+++ app.js
@@ -0,0 +1,43 @@
+const game = ()=>{
+let pScore=0;
+let cScore=0;
+
+//Start the game
+const startGame = ()=>{
+    const playBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+
+playBtn.addEventListener('click', ()=>{
+    introScreen.classList.add('fadeOut');
+    match.classList.add('fadeIn');
+});
+
+};
+
+//Play match
+const playMatch = ()=>{
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+   
+    //computer options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+    const computerNumber =Math.floor(Math.random()*3);
+    options.forEach(option=>{
+        options.addEventListener("click", function( ){
+    });
+});
+
+    computerOptions(computerNumber);
+
+
+//Is call all to inner functions
+
+startGame();
+playMatch();
+};
+
+//start the game funtion
+
+game();
